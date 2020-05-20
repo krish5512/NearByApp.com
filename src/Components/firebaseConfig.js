ebab2b2ccf49563b4b24b043eb56c1c5ffe9b753
KRISHNA KUAMR <krk5512@gmail.com>
2020-05-20 18:44:30 +0530

    Update firebaseConfig.js

--- src/Components/firebaseConfig.js
+++ src/Components/firebaseConfig.js
@@ -1,12 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCUX3ELi1rKuy8hRRdHxMoDffZW_4wwI6k",
-    authDomain: "nearbyapp-2020.firebaseapp.com",
-    databaseURL: "https://nearbyapp-2020.firebaseio.com",
-    projectId: "nearbyapp-2020",
-    storageBucket: "nearbyapp-2020.appspot.com",
-    messagingSenderId: "772535079509",
-    appId: "1:772535079509:web:c65186994512067939fadc",
-    measurementId: "G-FC7BS10P4F"
+//    Enter the firebase config details for the data
   };
 
   export default firebaseConfig;
